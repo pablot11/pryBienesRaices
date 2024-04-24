3bc4cfd3969970bd07513505c2547c1d3f17c96d
Pablo <pablo564903@gmail.com>
2024-04-24 20:43:50 -0300

    Correo

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -101,7 +101,6 @@ class PaginasController
             $mail->Body = $contenido;
             $mail->AltBody = 'Mail sobre bienes raices';
             //Enviar el email
-            debbug($mail);
             if ($mail->send()) {
                 $mensaje = "Mensaje enviado correctamente";
             } else {
